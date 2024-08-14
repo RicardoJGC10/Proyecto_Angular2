a0f8b5cf042127e9c2234f2d64377815b86778d1
RicardoJGC10 <chatotote10@gmail.com>
2024-08-14 14:28:30 -0600

    Ajustes Finales

--- src/app/layout/pruebas/pruebas.component.ts
+++ src/app/layout/pruebas/pruebas.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-pruebas',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './pruebas.component.html',
   styleUrl: './pruebas.component.css'
 })
 export class PruebasComponent {
+  toggleSidebar(event: Event): void {
+    event.preventDefault(); // Prevenir el comportamiento por defecto
+    // Lógica para colapsar/expandir el sidebar
+    const sidebar = document.querySelector('.app-sidebar');
+    if (sidebar) {
+      sidebar.classList.toggle('collapsed');
+    }
+  }
 
 }
